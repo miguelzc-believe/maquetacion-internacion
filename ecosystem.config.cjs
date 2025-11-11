6647b80163d5853a5e2edd925107ab0819d3d6f5
miguel zapata <miguel.zapata@believesol.com>
2025-11-11 15:34:56 -0400

    init config

---
+++ ecosystem.config.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  apps: [
+    {
+      name: "maquetacion-internacion",
+      script: "npm",
+      args: "run start",
+      instances: 1,
+      exec_mode: "fork",
+      watch: false,
+      autorestart: true,
+      max_memory_restart: "512M",
+      env: {
+        PORT: 5025,
+        NODE_ENV: "production",
+      },
+      error_file: "./logs/pm2-error.log",
+      out_file: "./logs/pm2-out.log",
+      log_date_format: "YYYY-MM-DD HH:mm:ss Z",
+      merge_logs: true,
+      time: true,
+    },
+  ],
+};
+
